10ed15f73fd544d1faa931a4f2a398865b379583
Oka Prasetiyo <55530474+okapras123@users.noreply.github.com>
2021-04-24 23:17:30 +0700

    Add files via upload

---
+++ export-cisco-config/export-config_via_ssh.py
@@ -0,0 +1,28 @@
+from netmiko import ConnectHandler
+import os
+
+router = {
+    'device_type':'cisco_ios',
+    'ip':'172.16.1.21',
+    'username':'automation',
+    'password':'ccna',
+    'secret':'cisco'
+    }
+
+conn = ConnectHandler(**router)
+try:
+    enable = conn.enable()
+    run_conf = conn.send_command('show running-config')
+    # you can define path for saving file manually, or automatic select path
+    
+    #path = os.path.join(os.getcwd(), 'export-config.txt')
+    path = os.path.join("C:\Python\Python37\myscipt", "export-config.txt")
+    file = open(dir, "w")
+    file.write(run_conf)
+    print(run_conf)
+    print(f"File already saved in {dir}")
+
+except Exception as Err:
+    print(Err)
+# print(conn.send_config_set(cmd))
+print("Selamat anda sudah berhasil membuat Loopback")
